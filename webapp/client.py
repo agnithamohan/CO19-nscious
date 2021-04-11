ef258f3441f52044edba8abd138279c37ac8433c
agnithamohan <agnitha.mohan@gmail.com>
2021-04-11 04:22:07 -0400

    webapp

--- webapp/client.py
+++ webapp/client.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 users = [{'uid': 0} , {'uid': 1} ] 
-
+h = 0 
 @app.route('/')
 def index():
-    return render_template('index.html', users=users)
+    menu = ['signup']
+    return render_template('index.html', menu=menu)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 
 
 @app.route('/info', methods=['GET', 'POST'])
 def info():
     data = request.get_json() 
-    name = data['name']
+    name = data['name'] 
+    name += str(h)
     return name
